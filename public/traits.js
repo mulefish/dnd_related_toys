6f034411ebbdb49cb7dd0cf4a03f2590126627f3
Paul Montgomery <finch.wren.swift@gmail.com>
2025-05-04 00:29:30 -0700

    6th pass

--- public/traits.js
+++ public/traits.js
@@ -8,7 +8,7 @@ class Orc {
         this.hitpoints = 0; // HP
         this.movement = 0; // Pixels movement across the board
         this.sight = 0; // Ablity to see across the board, in Pixels
-        this.initialtive = 0;
+        this.initiative = 0;
         // Keep track of 'score' for genetic algo across generations
         // Will figure the cross generation stuff later - right now this is here as a note to myself
         this.breedableScore = 0; 
@@ -45,35 +45,77 @@ function generateOrcName() {
 
     return name;
 }
-
+// NOTE: '_attributes' is globally available only for the TDD elf-test - otherwise it would be inide the assignPoints func: JS doesn't have the concepyt of 'protected'
+const _attributes = ["selfPreservation", "attack", "hitpoints", "movement", "initiative"];
 function assignPoints(orc, total_points) {
-    const attributes = ["selfPreservation", "attack", "hitpoints", "movement", "initialtive"];
+    // This is a '2 pass state machine'. First will distributes baseline valuesto each member of the class
+    // And the second pass will pass the 'spice' to each member of the class. 
+    // Each member in the '_attributes' array to get AT LEAST 10 and NO MORE than 100. 
+    // Except 'sight' which will be more than 'movement' and might well be up to 150. 
+    const _attributes = ["selfPreservation", "attack", "hitpoints", "movement", "initiative"];
     let remainingPoints = total_points;
 
-    // First pass: Assign each attribute between 10 and 30 points
-    attributes.forEach(attribute => {
+    _attributes.forEach(attribute => {
         let allocatedPoints = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
         allocatedPoints = Math.min(allocatedPoints, remainingPoints);
-        orc[attribute] += allocatedPoints;
+
+        orc[attribute] = Math.min(100, (orc[attribute] || 0) + allocatedPoints);
         remainingPoints -= allocatedPoints;
     });
 
-    // Ensure sight starts greater than movement
-    orc.sight = orc.movement + Math.floor(orc.movement * 0.5);
-    remainingPoints -= orc.sight - orc.movement;
-
-    // Second pass: Distribute remaining points (0 to 70) randomly
     while (remainingPoints > 0) {
-        let attribute = ["selfPreservation", "attack", "hitpoints", "movement", "sight"]
-            [Math.floor(Math.random() * 5)];
+        let attribute = _attributes[Math.floor(Math.random() * _attributes.length)];
         let allocatedPoints = Math.floor(Math.random() * (70 - 0 + 1));
 
         allocatedPoints = Math.min(allocatedPoints, remainingPoints);
-        orc[attribute] += allocatedPoints;
+
+        orc[attribute] = Math.min(100, (orc[attribute] || 0) + allocatedPoints);
         remainingPoints -= allocatedPoints;
     }
+    orc.sight = orc.movement + Math.floor(orc.movement * 0.5);
 }
+if (require.main === module) {
+    // TDD self-test!
+    // TODO: Move into a unit-test I suppose. Modern node has nice unit tests framework, 
+    // but I am both used to Jest AND I hate hate hate jest... Anyhow - I will unit-test and CICD up later
+    // For now, because I do not yet have a CICD pipeline set up, this is OK
+    const orcs = [];
+    for (let i = 0; i < 100; i++) {
+        const someOrc = new Orc(200);
+        orcs.push(someOrc);
+    }
 
-// Example usage
-let myOrc = new Orc(200);
-myOrc.displayStats();
+    // Goals:
+    // - No name is duplicated
+    // - All _attributes (except sight) are between 10 and 100
+    // - Sight is allowed to exceed 100 up to 150
+
+    const seenNames = new Set();
+    let isOk = true;
+
+    orcs.forEach((orc) => {
+        if (seenNames.has(orc.name)) {
+            console.log(`FAILBOT! name check dupe! "${orc.name}"`);
+            isOk = false;
+        } else {
+            seenNames.add(orc.name);
+        }
+
+        _attributes.forEach((attribute) => {
+            if (attribute !== "sight" && (orc[attribute] < 10 || orc[attribute] > 100)) {
+                console.log(`FAILBOT attribute check: ${attribute} = ${orc[attribute]} (should be between 10 and 100)`);
+                isOk = false;
+            }
+        });
+        if ( orc.sight < 10 || orc.sight > 150 ) {
+            console.log(`FAILBOT sight ought be to movement + movement * 0.5 and more than 10 and less than 151`);
+            isOk = false;
+        }
+   });
+
+    if (isOk) {
+        console.log("PASS: TDD says 'OK'!");
+    } else {
+        console.log("FAIL! Check it!");
+    }
+}
